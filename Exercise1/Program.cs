8909bd586b31c5b64aaab093112d07366f2a99c9
ahmed-razic <ahmed.razic@gmail.com>
2026-07-19 11:56:10 +0200

    commit

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            
+            int score1 = 0;
+            int score2 = 3;
+            int score3 = 4;
+            int score4 = 7;
+            int score5 = 8;
+
+            int[] scores;
+            bool[] states;
+            string[] names;
+
+            scores = new int[5];
+            states = new bool[5];
+            names = new string[5];
+
+            foreach (int score in scores)
+            {
+                Console.WriteLine(score);
+            }
+            foreach (bool state in states)
+            {
+                Console.WriteLine(state);
+            }
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            //Fill up arrays with values
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] = i+1;
+            }
+
+            foreach (int score in scores)
+            {
+                Console.WriteLine(score);
+            }
+
+            //Find minimum and maximum values in an array
+            int min = scores[0];
+            int max = scores[0];
+
+            foreach (int score in scores)
+            {
+                if (score < min)
+                    min = score;
+                if (score > max)
+                    max = score;
+            }
+
+            Console.WriteLine("Minimum score: {0}", min);
+            Console.WriteLine("Maximum score: {0}", max);
+
+            //Find the average of the values in an array
+            int[] array = new[] { 4, 51, 65, -55, 3, 99 };
+            int sum = 0;
+            foreach (int score in array)
+            {
+                sum += score;
+            }
+            Console.WriteLine($"Average score: {(double)sum / array.Length}");
         }
     }
 }
---
+++ ReplicatorOfArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ReplicatorOfArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[5];
+            int[] array2 = new int[5];
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write("Enter value for array1[{0}]: ", i);
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Copy values from array1 to array2
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array2[i] = array1[i];
+            }
+
+            // Display values in both arrays
+            Console.WriteLine("Values in array1:");
+            foreach (int value in array1)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine("Values in array2:");
+            foreach (int value in array2)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
